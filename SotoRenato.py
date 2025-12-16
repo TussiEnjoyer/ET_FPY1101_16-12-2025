a2536c50f219cbd82a588e064854b8134e273770
Ren <164810497+TussiEnjoyer@users.noreply.github.com>
2025-12-16 16:16:50 -0300

    Add files via upload

---
+++ SotoRenato.py
@@ -0,0 +1,38 @@
+import os
+os.system("cls")
+
+productos = {'8475HD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i5', 'Nvidia GTX1050'],
+'2175HD': ['lenovo', 14, '4GB', 'SSD', '512GB', 'Intel Core i5', 'Nvidia GTX1050'],
+'JjfFHD': ['Asus', 14, '16GB', 'SSD', '256GB', 'Intel Core i7', 'Nvidia RTX2080Ti'],
+'fgdxFHD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i3', 'integrada'],
+'GF75HD': ['Asus', 15.6, '8GB', 'DD', '1T', 'Intel Core i7', 'Nvidia GTX1050'],
+'123FHD': ['lenovo', 14, '6GB', 'DD', '1T', 'AMD Ryzen 5', 'integrada'],
+'342FHD': ['lenovo', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 7', 'Nvidia GTX1050'],
+'UWU131HD': ['Dell', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 3', 'Nvidia GTX1050'],
+}
+
+stock = {'8475HD': [387990,10], '2175HD': [327990,4], 'JjfFHD': [424990,1],
+'fgdxFHD': [664990,21], '123FHD': [290890,32], '342FHD': [444990,7],
+'GF75HD': [749990,2], 'UWU131HD': [349990,1], 'FS1230HD': [249990,0],}
+
+def stock_marca():
+    marca=input("ingrese marca a consultar: ")
+    if marca in productos:
+        print(productos(0))
+    return
+
+def búsqueda_precio(p_min, p_max):
+    input("ingrese precio minimo: ")
+    input("ingrese precio maximo: ")
+    return
+
+def actualizar_precio(modelo, p):
+    input("ingrese modelo a consultar: ")
+    input("ingrese precio nuevo: ")
+    return
+
+print("""\n*** MENU PRINCIPAL ***
+1. Stock marca.
+2. Búsqueda por precio.
+3. Actualizar precio.
+4. Salir.""")
